d6f2a64ba9f1930e8dd6905c7e3c2ba81dd529bb
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-05-24 14:13:51 +0200

    auth: implement login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User.model");
 
@@ -10,9 +11,9 @@ const saltRounds = 10;
 
 
 
-// POST /auth/signup
+// POST /auth/signup - Register
 router.post('/signup', (req, res, next) => {
-  
+
   const { email, password, name } = req.body;
 
   // Check if the email or password or name is provided as an empty string 
@@ -75,6 +76,59 @@ router.post('/signup', (req, res, next) => {
       res.status(500).json({ message: "Internal Server Error" })
     });
 });
+
+
+
+
+// POST  /auth/login - Verifies email and password and returns a JWT
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check if email or password are provided as empty string 
+  if (email === '' || password === '') {
+    res.status(400).json({ message: "Provide email and password." });
+    return;
+  }
+
+  // Check the users collection if a user with the same email exists
+  User.findOne({ email })
+    .then((foundUser) => {
+
+      if (!foundUser) {
+        // If the user is not found, send an error response
+        res.status(401).json({ message: "User not found." })
+        return;
+      }
+
+      // Compare the provided password with the one saved in the database
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+      if (passwordCorrect) {
+        // Deconstruct the user object to omit the password
+        const { _id, email, name } = foundUser;
+
+        // Create an object that will be set as the token payload
+        const payload = { _id, email, name };
+
+        // Create and sign the token
+        const authToken = jwt.sign(
+          payload,
+          process.env.TOKEN_SECRET,
+          { algorithm: 'HS256', expiresIn: "6h" }
+        );
+
+        // Send the token as the response
+        res.status(200).json({ authToken: authToken });
+      }
+      else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+
+    })
+    .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+});
+
+
 
 
 module.exports = router;
